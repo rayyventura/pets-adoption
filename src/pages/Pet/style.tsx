8c6a3100daa891ae6dae2b8ad24df0320066a7a5
Rayane Ventura <rayyventura@gmail.com>
2022-05-26 05:08:52 +0200

    adjust layout

--- src/pages/Pet/style.tsx
+++ src/pages/Pet/style.tsx
@@ -59,7 +59,7 @@ const LeftContainer = styled.div`
     width: 100%;
     align-items: center;
     justify-content: flex-start;
-    margin-top: 80px;
+    margin-top: 100px;
   }
 `;
 const RightContainer = styled.div`
